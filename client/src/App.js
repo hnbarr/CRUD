9d307a6bb6c546a320355a2daaf93800ebd95366
Heather Barr <hnbarr@yahoo.com>
2019-06-21 15:47:15 -0500

    router done somewhat but broke the app myst check into that

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import ListOfUsers from "./components/ListOfUsers";
 import NewModel from "./components/NewModel";
+// import { BrowserRouter } from 'react-router-dom'
+// import Router from './router'
 
 class App extends Component {
   constructor() {
     super();
   }
   componentDidMount() {
-    
+
   }
   render() {
     return (
       <div>
+      {/* <BrowserRouter> */}
         Client
         <ListOfUsers />
         <NewModel />
+      {/* </BrowserRouter> */}
       </div>
     );
   }
---
+++ client/src/router.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import ListOfUsers from './components/ListOfUsers'
+import NewModel from './components/NewModel'
+import UserDetail from './components/UserDetail'
+
+const Router = () => {
+    return (
+        // <Switch>
+        //     <Route path='/ListOfUsers' component={ListOfUsers}></Route>
+        //     <Route path='/NewModel' component={NewModel}></Route>
+        //     <Route path='/UserDetail' component={UserDetail}></Route>
+        // </Switch>
+        <div>
+            router here
+        </div>
+    );
+}
+
+export default Router
